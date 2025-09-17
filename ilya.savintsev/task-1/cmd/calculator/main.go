c99f83fe78e7ac094707a844df024ab3da119537
faxryzen <faxryzen@users.noreply.github.com>
2025-09-17 22:13:15 +0300

    [TASK-1] Initial commit for task-1

---
+++ ilya.savintsev/task-1/cmd/calculator/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var x, y int
+	var operator string
+	_, err1 := fmt.Scanln(&x)
+	_, err2 := fmt.Scanln(&y)
+	_, err3 := fmt.Scanln(&operator)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		os.Exit(1)
+  }
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		os.Exit(1)
+  }
+	if err3 != nil || (operator != "+" && operator != "-" && operator != "*" && operator != "/") {
+		fmt.Println("Invalid operation")
+		os.Exit(1)
+  }
+	if operator == "/" && y == 0 {
+		fmt.Println("Division by zero")
+		os.Exit(1)
+  }
+	switch (operator) {
+	case "+":
+		fmt.Println(x + y)
+	case "-":
+		fmt.Println(x - y)
+	case "*":
+		fmt.Println(x * y)
+	case "/":
+		fmt.Println(x / y)
+	}
+}
